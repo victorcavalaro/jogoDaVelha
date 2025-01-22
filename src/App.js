c303c7566d3049cbe3446bf6d877a1f7138162cd
victorcavalaro <victorcavalaro298@gmail.com>
2025-01-22 10:57:26 -0300

    codigo final

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i){
     if(calculateWinner(squares) || squares[i]){
       return;
@@ -22,9 +19,9 @@ export default function Board() {
     }else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-
+    onPlay(nextSquares)
+  }
+  
     const winner = calculateWinner(squares);
     let status;
     if (winner){
@@ -54,7 +51,28 @@ export default function Board() {
       </>
     );
   }; 
-}
+
+  export default function Game(){
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquare){
+      setHistory([...history, nextSquare]);
+      setXIsNext(!xIsNext);
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    );
+  }
 
 function calculateWinner(squares){
   const lines = [
